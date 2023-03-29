aa1955e780b3bc13d2f81ab6b0ebc81b1e5abe5c
nobodomon <nobodomon@gmail.com>
2023-03-29 22:54:57 +0800

    ewfswetf

--- src/main/java/ReviewMapper.java
+++ src/main/java/ReviewMapper.java
@@ -25,13 +25,25 @@ public class ReviewMapper extends Mapper<LongWritable, Text, Text, MapWritable> 
 
         String[] fields = line.split(",");
 
-        String summary = fields[23].trim();
-
         String pros = fields[19].trim();
+
+        if(pros.isEmpty()){
+            pros = fields[20].trim();
+        }
 
         String cons = fields[21].trim();
 
-        if(summary == ""){
+        if(cons.isEmpty()){
+            cons = fields[22].trim();
+        }
+
+        String summary = fields[23].trim();
+
+        if(summary.isEmpty()){
+            summary = fields[24].trim();
+        }
+
+        if(summary.isEmpty()){
             summarySentiment = new IntWritable(2);
         }else{
             summarySentiment = new IntWritable(Integer.parseInt(nlpPipeline.findSentiment(summary)[1]));
@@ -39,7 +51,7 @@ public class ReviewMapper extends Mapper<LongWritable, Text, Text, MapWritable> 
 
         mapWritable.put(new Text("summary"), summarySentiment);
 
-        if(pros == ""){
+        if(pros.isEmpty()){
             prosSentiment = new IntWritable(2);
         }else{
             prosSentiment = new IntWritable(Integer.parseInt(nlpPipeline.findSentiment(pros)[1]));
@@ -47,7 +59,7 @@ public class ReviewMapper extends Mapper<LongWritable, Text, Text, MapWritable> 
 
         mapWritable.put(new Text("pros"), prosSentiment);
 
-        if(cons == ""){
+        if(cons.isEmpty()){
             consSentiment = new IntWritable(2);
         }else{
             consSentiment = new IntWritable(Integer.parseInt(nlpPipeline.findSentiment(cons)[1]));
